989492c31158d8c12371c4524f03ac4707ee2876
Sushant <cashu853@gmail.com>
2024-04-16 10:50:43 +0530

    E-comerse ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header';
+import Home from './components/Home';
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function NavScrollExample() {
+
+  return (
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" >
+      <Container fluid>
+        <Navbar.Brand href="#" className='ms-4'> LOGO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto  my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+           
+
+            <Nav.Link href="#" >
+              Contact
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex me-4">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+
+export default NavScrollExample;
