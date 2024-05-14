f1b92829ef1d77dfcc3bb017ff9b176b1a882b50
Sushant <cashu853@gmail.com>
2024-05-14 14:24:03 +0530

    FILTER FUNCTIONALITY

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Contact() {
   return (
     <div className=' d-flex align-content-center justify-content-center'>
-      This is Contact
+      This is Contact 
     </div>
   )
 }
