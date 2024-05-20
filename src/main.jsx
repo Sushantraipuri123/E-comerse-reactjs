64fac36ae62aefeb4582ca81af3cb0297db62760
Sushant <cashu853@gmail.com>
2024-05-20 14:37:57 +0530

    Added Sign up and Sign in Pages

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import Women from './components/categories/Women';
 import Electronics from './components/categories/Electronics';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import SignUp from './components/SignUp';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -29,6 +30,7 @@ ReactDOM.render(
           <Route path="men" element={<Men/>} />
           <Route path="women" element={<Women/>} />
           <Route path="electronics" element={<Electronics/>} />
+          <Route path="signup" element={<SignUp/>} />
         </Route>
       </Routes>
     </Router>
